d0b2d1b01e5d6b1a05992e05ffbb5ada5fdc47ca
Phu Ha <phuha.engineer@gmail.com>
2023-12-03 11:24:45 -0500

    added JavaFX libraries.

--- UIwithjavafx/src/module-info.java
+++ UIwithjavafx/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module UIwithjavafx {
+	requires javafx.graphics;
 }
--- UIwithjavafx/src/uiwithjavafx/UserInterface.java
+++ UIwithjavafx/src/uiwithjavafx/UserInterface.java
@@ -1,6 +1,10 @@
 package uiwithjavafx;
 
-public class UserInterface {
+import javafx.application.Application;
+
+public class UserInterface extends Application {
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
